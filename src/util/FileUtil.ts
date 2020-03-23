279e3415939af584402fcd96687f10cba0a05b95
ipmanlk <io@navinda.xyz>
2020-03-24 00:14:23 +0530

    add: getFileSize to fileUtil

--- src/util/FileUtil.ts
+++ src/util/FileUtil.ts
@@ -1,16 +1,33 @@
-import { readFileSync } from "fs";
+import { readFileSync, statSync } from "fs";
 
 class FileUtil {
-    static async readFile(path) {
+    static async readFileAsBuffer(path) {
         try {
-            return readFileSync(path) as Buffer;
+            let fileContent = readFileSync(path);
+            return (fileContent as Buffer);
         } catch (e) {
-			console.log(e);
-			throw {
-				status: false,
-				type: "server",
-				msg: "Server Error!. Please check logs."
-			}
+            console.log(e);
+            throw {
+                status: false,
+                type: "server",
+                msg: "Server Error!. Please check logs."
+            }
+        }
+    }
+
+    static async getFileSize(path, unit) {
+        try {
+            const stats = statSync(path);
+            const fileSizeInBytes = stats["size"];
+            const unitSizes = { Byte: 1, KB: 1024, MB: 1048576, GB: 1073741824 };
+            return fileSizeInBytes / unitSizes[unit];
+        } catch (e) {
+            console.log(e);
+            throw {
+                status: false,
+                type: "server",
+                msg: "Server Error!. Please check logs."
+            }
         }
     }
 }
