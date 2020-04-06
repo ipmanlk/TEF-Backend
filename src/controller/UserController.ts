3eca08dd5e217a731e8aed6cfe636a896b9841f0
ipmanlk <io@navinda.xyz>
2020-04-06 12:28:39 +0530

    add: getAll for userController

---
+++ src/controller/UserController.ts
@@ -0,0 +1,203 @@
+import { getRepository } from "typeorm";
+import { User } from "../entity/User";
+
+class UserController {
+	static async getOne({ id }) {
+
+		// search for an entry with given id
+		const user = await getRepository(User).findOne({
+			id: id
+		}).catch(e => {
+			console.log(e.code, e);
+			throw {
+				status: false,
+				type: "server",
+				msg: "Server Error!. Please check logs."
+			};
+		});
+
+		// check if entry exists
+		if (user !== undefined) {
+			return {
+				status: true,
+				data: user
+			};
+		} else {
+			throw {
+				status: false,
+				type: "input",
+				msg: "Unable to find an user with that id."
+			};
+		}
+	}
+
+	static async getAll() {
+		const users = await getRepository(User).find({
+			select: ["id", "employeeId", "username","userStatusId", "employeeCreatedId", "docreation", "description", "roleId"],
+			relations: ["userStatus", "role", "employeeCreated", "employee"]
+		}).catch(e => {
+			console.log(e.code, e);
+			throw {
+				status: false,
+				type: "server",
+				msg: "Server Error!. Please check logs."
+			};
+		});
+
+        // remove useless attributes
+        const usersFiltered = users.map(user => {
+            return {
+                id: user.id,
+                username: user.username,
+                employee: {
+                    number: user.employee.number
+                },
+                employeeCreated: {
+                    number: user.employeeCreated.number
+                },
+                role: {
+                    name: user.role.name
+                },
+                docreation: user.docreation,
+                userStatus: {
+                    name: user.userStatus.name
+                }
+            }
+        });
+
+		return {
+			status: true,
+			data: usersFiltered
+		};
+	}
+
+	// static async save(data) {
+	// 	// create user object
+	// 	const user = data as user;
+
+	// 	// extract photo
+	// 	const { photo } = data;
+
+	// 	// // calculate photo size in kb
+	// 	// const photoSize = photo.length / 999;
+
+	// 	// if (photoSize > 500) {
+	// 	// 	throw {
+	// 	// 		status: false,
+	// 	// 		type: "input",
+	// 	// 		msg: "Your photo should be smaller than 500KB."
+	// 	// 	}
+	// 	// }
+
+	// 	// read photo as buffer
+	// 	const decodedBase64 = this.decodeBase64Image(photo);
+	// 	user.photo = decodedBase64.data;
+
+	// 	// save to db
+	// 	await getRepository(user).save(user).catch(e => {
+	// 		console.log(e.code, e);
+
+	// 		if (e.code == "ER_DUP_ENTRY") {
+	// 			throw {
+	// 				status: false,
+	// 				type: "input",
+	// 				msg: "Entry with that user number already exists!."
+	// 			}
+	// 		}
+	// 		throw {
+	// 			status: false,
+	// 			type: "server",
+	// 			msg: "Server Error!. Please check logs."
+	// 		}
+	// 	});
+
+	// 	return {
+	// 		status: true,
+	// 		msg: "That user has been added!"
+	// 	};
+	// }
+
+	// static async update(data) {
+	// 	// create user object
+	// 	const editeduser = data as user;
+
+	// 	// check if user is present with the given id
+	// 	const selecteduser = await getRepository(user).findOne(editeduser.id).catch(e => {
+	// 		console.log(e.code, e);
+	// 		throw {
+	// 			status: false,
+	// 			type: "server",
+	// 			msg: "Server Error!. Please check logs."
+	// 		}
+	// 	});
+
+	// 	if (!selecteduser) {
+	// 		throw {
+	// 			status: false,
+	// 			type: "input",
+	// 			msg: "That user doesn't exist in our database!."
+	// 		}
+	// 	}
+
+	// 	// check if photo has changed
+	// 	if (data.photo == false) {
+	// 		editeduser.photo = selecteduser.photo;
+	// 	} else {
+	// 		// read photo as buffer
+	// 		const decodedBase64 = this.decodeBase64Image(editeduser.photo);
+	// 		editeduser.photo = decodedBase64.data;
+	// 	}
+
+	// 	// update the user
+	// 	await getRepository(user).save(editeduser).catch(e => {
+	// 		console.log(e.code, e);
+	// 		throw {
+	// 			status: false,
+	// 			type: "server",
+	// 			msg: "Server Error!. Please check logs."
+	// 		}
+	// 	});
+
+	// 	return {
+	// 		status: true,
+	// 		msg: "That user has been updated!"
+	// 	};
+	// }
+
+	// static async delete({ id }) {
+	// 	// find user with the given id
+	// 	const user = await getRepository(user).findOne({ id: id }).catch(e => {
+	// 		console.log(e.code, e);
+	// 		throw {
+	// 			status: false,
+	// 			type: "server",
+	// 			msg: "Server Error!. Please check logs."
+	// 		}
+	// 	});
+
+	// 	if (!user) {
+	// 		throw {
+	// 			status: false,
+	// 			type: "input",
+	// 			msg: "That user doesn't exist in our database!."
+	// 		}
+	// 	}
+
+	// 	// delete the user
+	// 	await getRepository(user).delete(user).catch(e => {
+	// 		console.log(e.code, e);
+	// 		throw {
+	// 			status: false,
+	// 			type: "server",
+	// 			msg: "Server Error!. Please check logs."
+	// 		}
+	// 	});
+
+	// 	return {
+	// 		status: true,
+	// 		msg: "That user has been deleted!"
+	// 	};
+	// }
+}
+
+export default UserController;
