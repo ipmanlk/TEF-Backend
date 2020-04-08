f52d2f6a65469eccf90f96dd883231b7b80b5a71
ipmanlk <io@navinda.xyz>
2020-04-08 20:12:11 +0530

    change: cleanup employee dao search query builder

--- src/dao/EmployeeDao.ts
+++ src/dao/EmployeeDao.ts
@@ -5,12 +5,12 @@ export class EmployeeDao {
     static search({ keyword, skip }) {
         return getRepository(Employee)
             .createQueryBuilder("e")
+            .select([
+                "e.id", "e.number", "e.fullName", "e.callingName", "e.nic", "e.mobile"
+            ])
             .leftJoinAndSelect("e.designation", "de")
             .leftJoinAndSelect("e.civilStatus", "cs")
             .leftJoinAndSelect("e.employeeStatus", "es")
-            .select([
-                "e.id", "e.number", "e.fullName", "e.callingName", "e.nic", "e.mobile", "de", "cs", "es"
-            ])
             .where("e.number LIKE :keyword", { keyword: `%${keyword}%` })
             .orWhere("e.fullName LIKE :keyword", { keyword: `%${keyword}%` })
             .orWhere("e.callingName LIKE :keyword", { keyword: `%${keyword}%` })
