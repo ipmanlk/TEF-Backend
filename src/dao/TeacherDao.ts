bb465fbcc49eb9045fcf6be4b35e959bd6baeec7
ipmanlk <io@navinda.xyz>
2020-04-08 16:04:17 +0530

    removed: invalid class from prev tests

--- src/dao/TeacherDao.ts
+++
@@ -1,9 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Teacher } from "../entity/Teacher";
-
-@EntityRepository(Teacher)
-export class UserRepository extends Repository<Teacher> {
-    findById(id: number) {
-        return this.findOne({ id: id});
-    }
-}
