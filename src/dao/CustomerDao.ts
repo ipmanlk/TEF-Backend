4ad61fc66b6dd33c0e7e256b71e519b22e5d761e
ipmanlk <io@navinda.xyz>
2020-08-12 14:34:14 +0530

    fix: customer dao

--- src/dao/CustomerDao.ts
+++ src/dao/CustomerDao.ts
@@ -7,14 +7,16 @@ export class CustomerDao {
             .createQueryBuilder("c")
             .leftJoinAndSelect("c.employee", "ce")
             .select([
-                "c.id", "c.name", "c.address", "c.nic", "c.genderId", "c.mobile", "c.land", "c.email", "c.customerStatusId", "c.description", "c.doregistration", "ce.id", "ce.number"
+                "c.id", "c.cname", "c.number", "c.cmobile", "c.toBePaid", "c.points" , "c.cpname", "c.cpmobile", "c.address", "c.nic", "c.genderId", "c.email", "c.customerStatusId", "c.description", "c.doregistration", "ce.id", "ce.number"
             ])
-            .leftJoinAndSelect("c.customerStatus", "cs")            
-            .where("c.name LIKE :keyword", { keyword: `%${keyword}%` })
+            .leftJoinAndSelect("c.customerStatus", "cs")
+            .leftJoinAndSelect("c.customerType", "ct")
+            .where("c.cname LIKE :keyword", { keyword: `%${keyword}%` })
             .orWhere("c.address LIKE :keyword", { keyword: `%${keyword}%` })
             .orWhere("c.nic LIKE :keyword", { keyword: `%${keyword}%` })
             .orWhere("c.email LIKE :keyword", { keyword: `%${keyword}%` })
             .orWhere("cs.name LIKE :keyword", { keyword: `%${keyword}%` })
+            .orWhere("ct.name LIKE :keyword", { keyword: `%${keyword}%` })
             .skip(skip)
             .take(15)
             .getMany()
