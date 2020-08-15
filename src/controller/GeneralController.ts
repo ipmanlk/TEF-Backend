92fd92c8cf447c361a1746760405225c96d52e27
ipmanlk <io@navinda.xyz>
2020-08-15 11:07:29 +0530

    add: product sub tables to general controller allowed list

--- src/controller/GeneralController.ts
+++ src/controller/GeneralController.ts
@@ -3,7 +3,7 @@ import { getRepository } from "typeorm";
 export class GeneralController {
     static async get(data) {
 
-        const generalTables = ["gender", "civil_status", "module", "user_status", "customer_status", "customer_type", "material_status", "material_type", "unit_type", "risk_category"];
+        const generalTables = ["gender", "civil_status", "module", "user_status", "customer_status", "customer_type", "material_status", "material_type", "unit_type", "risk_category", "product_status", "category", "product_type"];
 
         const table = data.table;
 
