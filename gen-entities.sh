51520c4f3f5e96721f83217297d43bd0717efaa9
ipmanlk <io@navinda.xyz>
2020-09-22 19:51:00 +0530

    add: entity generator script

--- gen-entities.sh
+++ gen-entities.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npx typeorm-model-generator -h localhost -d twoelephantsfireworks -u root -x "" -e mysql -o /tmp/
+npx typeorm-model-generator -h localhost -d twoelephantsfireworks -u root -x "root" -e mysql -o /tmp/
 mv /tmp/entities/* ./src/entity
