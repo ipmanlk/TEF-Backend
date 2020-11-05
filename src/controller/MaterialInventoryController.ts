49df84ed913b4a24f9772a48fc6b25e1b1d4502f
ipmanlk <io@navinda.xyz>
2020-11-05 11:20:30 +0530

    add: unitType to material inventory dao getAll

--- src/controller/MaterialInventoryController.ts
+++ src/controller/MaterialInventoryController.ts
@@ -5,7 +5,7 @@ export class MaterialInventoryController {
   static async get(data) {
     return this.search(data);
   }
-  
+
   private static async search(data = {}) {
     const entries = await MaterialInventoryDao.search(data).catch(e => {
       console.log(e.code, e);
--- src/dao/MaterialInventoryDao.ts
+++ src/dao/MaterialInventoryDao.ts
@@ -3,19 +3,28 @@ import { MaterialInventory } from "../entity/MaterialInventory";
 
 export class MaterialInventoryDao {
   static search({ keyword = "", skip = 0, limit = 15 }) {
-    return getRepository(MaterialInventory)
-      .createQueryBuilder("mi")
-      .leftJoinAndSelect("mi.materialInventoryStatus", "mis")
-      .leftJoinAndSelect("mi.material", "m")
-      .where("m.name LIKE :keyword", { keyword: `%${keyword}%` })
-      .orWhere("m.code LIKE :keyword", { keyword: `%${keyword}%` })
-      .orWhere("m.unitPrice LIKE :keyword", { keyword: `%${keyword}%` })
-      .orWhere("m.addedDate LIKE :keyword", { keyword: `%${keyword}%` })
-      .orWhere("mi.qty LIKE :keyword", { keyword: `%${keyword}%` })
-      .orWhere("mi.availableQty LIKE :keyword", { keyword: `%${keyword}%` })
-      .orWhere("mis.name LIKE :keyword", { keyword: `%${keyword}%` })
-      .skip(skip)
-      .take(limit)
-      .getMany()
+    if (limit != 0) {
+      return getRepository(MaterialInventory)
+        .createQueryBuilder("mi")
+        .leftJoinAndSelect("mi.materialInventoryStatus", "mis")
+        .leftJoinAndSelect("mi.material", "m")
+        .where("m.name LIKE :keyword", { keyword: `%${keyword}%` })
+        .orWhere("m.code LIKE :keyword", { keyword: `%${keyword}%` })
+        .orWhere("m.unitPrice LIKE :keyword", { keyword: `%${keyword}%` })
+        .orWhere("m.addedDate LIKE :keyword", { keyword: `%${keyword}%` })
+        .orWhere("mi.qty LIKE :keyword", { keyword: `%${keyword}%` })
+        .orWhere("mi.availableQty LIKE :keyword", { keyword: `%${keyword}%` })
+        .orWhere("mis.name LIKE :keyword", { keyword: `%${keyword}%` })
+        .skip(skip)
+        .take(limit)
+        .getMany()
+    } else {
+      return getRepository(MaterialInventory)
+        .createQueryBuilder("mi")
+        .leftJoin("mi.material", "m")
+        .addSelect(["m.id", "m.code", "m.name"])
+        .leftJoinAndSelect("m.unitType", "ut")
+        .getMany()
+    }
   }
 }
