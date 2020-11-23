eb86177d95f110f378470cb5ce27d3330ce95613
ipmanlk <io@navinda.xyz>
2020-11-23 13:05:09 +0530

    add: queries for day, week, month, quarter and year for sales report

--- src/controller/ReportController.ts
+++ src/controller/ReportController.ts
@@ -11,6 +11,17 @@ export class ReportController {
     const endDate = moment(end).format("YYYY-MM-DD");
 
     switch (type) {
+      case "day":
+        query = `
+        SELECT DATE(added_date) AS sales_date,
+         SUM(net_total) AS net_total,
+         SUM(payed_amount) AS payed_amount,
+         COUNT(*) AS transactions
+        FROM customer_invoice
+        GROUP BY DATE(added_date)
+        ORDER BY DATE(added_date)
+        `;
+        break;
       case "week":
         query = `
         SELECT FROM_DAYS(TO_DAYS(added_date) -MOD(TO_DAYS(added_date) -1, 7)) AS week_beginning,
@@ -18,12 +29,45 @@ export class ReportController {
          SUM(payed_amount) AS payed_amount,
          COUNT(*) AS transactions
         FROM customer_invoice
-        WHERE added_date >= "2020-11-01" AND added_date <= "2020-11-15"
+        WHERE added_date >= "${startDate}" AND added_date <= "${endDate}"
         GROUP BY FROM_DAYS(TO_DAYS(added_date) -MOD(TO_DAYS(added_date) -1, 7))
         ORDER BY FROM_DAYS(TO_DAYS(added_date) -MOD(TO_DAYS(added_date) -1, 7))
-        `
+        `;
         break;
-
+      case "month":
+        query = `
+        SELECT DATE(DATE_FORMAT(added_date, '%Y-%m-01')) AS month_beginning,
+         SUM(net_total) AS net_total,
+         SUM(payed_amount) AS payed_amount,
+         COUNT(*) AS transactions
+        FROM customer_invoice
+        WHERE added_date >= "${startDate}" AND added_date <= "${endDate}"
+        GROUP BY DATE(DATE_FORMAT(added_date, '%Y-%m-01'))
+        ORDER BY DATE(DATE_FORMAT(added_date, '%Y-%m-01'))
+        `;
+        break;
+      case "quarter":
+        query = `
+        SELECT DATE(CONCAT(YEAR(added_date),'-', 1 + 3*(QUARTER(added_date)-1),'-01')) AS quarter_beginning,
+          SUM(net_total) AS net_total,
+          SUM(payed_amount) AS payed_amount,
+          COUNT(*) AS transactions
+        FROM customer_invoice
+        WHERE added_date >= "${startDate}" AND added_date <= "${endDate}"
+        GROUP BY DATE(CONCAT(YEAR(added_date),'-', 1 + 3*(QUARTER(added_date)-1),'-01'))
+        ORDER BY DATE(CONCAT(YEAR(added_date),'-', 1 + 3*(QUARTER(added_date)-1),'-01'))
+          `;
+        break;
+      case "year":
+        query = `
+        SELECT YEAR(added_date) as year,
+	        SUM(net_total) AS net_total,
+          SUM(payed_amount) AS payed_amount,
+          COUNT(*) AS transactions
+          FROM customer_invoice
+          WHERE added_date >= "${startDate}" AND added_date <= "${endDate}"
+          GROUP BY YEAR(added_date)
+      `;
       default:
         break;
     }
