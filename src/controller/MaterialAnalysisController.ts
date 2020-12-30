9d091b3dcd2d248630c781676c737807d8e04bbc
ipmanlk <io@navinda.xyz>
2020-12-30 14:55:07 +0530

    add: unit price to material analysis controller

--- src/controller/MaterialAnalysisController.ts
+++ src/controller/MaterialAnalysisController.ts
@@ -2,73 +2,78 @@ import { getRepository } from "typeorm";
 import { MaterialAnalysis } from "../entity/MaterialAnalysis";
 
 export class MaterialAnalysisController {
+	static async getOne(productId) {
+		// find material details for given product
+		const materialInfo = await getRepository(MaterialAnalysis)
+			.find({
+				where: { productId: productId },
+				relations: ["unitType", "material"],
+			})
+			.catch((e) => {
+				console.log(e.code, e);
+				throw {
+					status: false,
+					type: "server",
+					msg: "Server Error!. Please check logs.",
+				};
+			});
 
-  static async getOne(productId) {
+		// extract useful data
+		const productMaterials = materialInfo.map((m) => {
+			return {
+				material: {
+					id: m.material.id,
+					code: m.material.code,
+					name: m.material.name,
+					unitPrice: m.material.unitPrice,
+				},
+				amount: m.amount,
+				unitType: m.unitType,
+			};
+		});
 
-    // find material details for given product
-    const materialInfo = await getRepository(MaterialAnalysis).find({
-      where: { productId: productId },
-      relations: ["unitType", "material"]
-    }).catch(e => {
-      console.log(e.code, e);
-      throw {
-        status: false,
-        type: "server",
-        msg: "Server Error!. Please check logs."
-      };
-    });
+		return {
+			status: true,
+			data: productMaterials,
+		};
+	}
 
-    // extract useful data
-    const productMaterials = materialInfo.map(m => {
-      return {
-        material: {
-          id: m.material.id,
-          code: m.material.code,
-          name: m.material.name
-        },
-        amount: m.amount,
-        unitType: m.unitType
-      }
-    })
+	static async update(data) {
+		// remove existing records
+		await getRepository(MaterialAnalysis)
+			.createQueryBuilder()
+			.delete()
+			.where("productId = :id", { id: data.productId })
+			.execute()
+			.catch((e) => {
+				console.log(e.code, e);
+				throw {
+					status: false,
+					type: "server",
+					msg: "Server Error!. Please check logs.",
+				};
+			});
 
-    return {
-      status: true,
-      data: productMaterials
-    }
-  }
+		try {
+			// save product mateirals
+			for (let pm of data.productMaterials) {
+				await getRepository(MaterialAnalysis).save({
+					...pm,
+					productId: data.productId,
+				});
+			}
 
-  static async update(data) {
-    // remove existing records
-    await getRepository(MaterialAnalysis).createQueryBuilder()
-      .delete()
-      .where("productId = :id", { id: data.productId })
-      .execute()
-      .catch(e => {
-        console.log(e.code, e);
-        throw {
-          status: false,
-          type: "server",
-          msg: "Server Error!. Please check logs."
-        };
-      });
-
-    try {
-      // save product mateirals
-      for (let pm of data.productMaterials) {
-        await getRepository(MaterialAnalysis).save({ ...pm, productId: data.productId });
-      }
-
-      return {
-        status: true,
-        msg: "Product materials have been updated!"
-      };
-    } catch (e) {
-      console.log(e.code, e);
-      throw {
-        status: false,
-        type: "server",
-        msg: "Server Error!. Please check logs."
-      }
-    }
-  }
+			return {
+				status: true,
+				msg: "Product materials have been updated!",
+			};
+		} catch (e) {
+			console.log(e.code, e);
+			throw {
+				status: false,
+				type: "server",
+				msg: "Server Error!. Please check logs.",
+			};
+		}
+	}
 }
