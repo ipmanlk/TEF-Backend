2acd99698b1e18e5259c1dc73214ef965518ba74
ipmanlk <io@navinda.xyz>
2021-04-05 13:27:41 +0530

    add: pdf sending to purchase order

--- src/controller/PurchaseOrderController.ts
+++ src/controller/PurchaseOrderController.ts
@@ -1,3 +1,6 @@
+import * as puppeteer from "puppeteer";
+import fetch from "node-fetch";
+
 import { getRepository } from "typeorm";
 import { PurchaseOrder } from "../entity/PurchaseOrder";
 import { PurchaseOrderMaterial } from "../entity/PurchaseOrderMaterial";
@@ -6,270 +9,333 @@ import { Quotation } from "../entity/Quotation";
 import { QuotationStatus } from "../entity/QuotationStatus";
 import { PurchaseOrderDao } from "../dao/PurchaseOrderDao";
 import { MiscUtil } from "../util/MiscUtil";
+import { EmailController } from ".";
 
 export class PurchaseOrderController {
+	static async get(data) {
+		if (data !== undefined && data.id) {
+			return this.getOne(data);
+		} else {
+			return this.search(data);
+		}
+	}
 
-  static async get(data) {
-    if (data !== undefined && data.id) {
-      return this.getOne(data);
-    } else {
-      return this.search(data);
-    }
-  }
+	private static async getOne({ id }) {
+		// search for an entry with given id
+		const entry = await PurchaseOrderDao.getOne(id).catch((e) => {
+			console.log(e.code, e);
+			throw {
+				status: false,
+				type: "server",
+				msg: "Server Error!. Please check logs.",
+			};
+		});
 
-  private static async getOne({ id }) {
-    // search for an entry with given id
-    const entry = await PurchaseOrderDao.getOne(id).catch(e => {
-      console.log(e.code, e);
-      throw {
-        status: false,
-        type: "server",
-        msg: "Server Error!. Please check logs."
-      };
-    });
+		// check if entry exists
+		if (entry !== undefined) {
+			// remove useless elements
+			entry[
+				"createdEmployee"
+			] = `${entry.employee.callingName} (${entry.employee.number})`;
+			delete entry.employee;
 
-    // check if entry exists
-    if (entry !== undefined) {
-      // remove useless elements
-      entry["createdEmployee"] = `${entry.employee.callingName} (${entry.employee.number})`;
-      delete entry.employee;
+			return {
+				status: true,
+				data: entry,
+			};
+		} else {
+			throw {
+				status: false,
+				type: "input",
+				msg: "Unable to find an entry with that id.",
+			};
+		}
+	}
 
-      return {
-        status: true,
-        data: entry
-      };
-    } else {
-      throw {
-        status: false,
-        type: "input",
-        msg: "Unable to find an entry with that id."
-      };
-    }
-  }
+	private static async search(data = {}) {
+		const entries = await PurchaseOrderDao.search(data).catch((e) => {
+			console.log(e.code, e);
+			throw {
+				status: false,
+				type: "server",
+				msg: "Server Error!. Please check logs.",
+			};
+		});
 
-  private static async search(data = {}) {
-    const entries = await PurchaseOrderDao.search(data).catch(e => {
-      console.log(e.code, e);
-      throw {
-        status: false,
-        type: "server",
-        msg: "Server Error!. Please check logs."
-      }
-    });
+		return {
+			status: true,
+			data: entries,
+		};
+	}
 
-    return {
-      status: true,
-      data: entries
-    };
-  }
+	static async save(data, session) {
+		// create purchase order code
+		const lastEntry = await getRepository(PurchaseOrder).findOne({
+			select: ["id", "pocode"],
+			order: { id: "DESC" },
+		});
 
-  static async save(data, session) {
+		if (lastEntry) {
+			data.pocode = MiscUtil.getNextNumber("PUR", lastEntry.pocode, 5);
+		} else {
+			data.pocode = MiscUtil.getNextNumber("PUR", undefined, 5);
+		}
 
-    // create purchase order code
-    const lastEntry = await getRepository(PurchaseOrder).findOne({
-      select: ["id", "pocode"],
-      order: { id: "DESC" }
-    });
+		// create a purchase order object
+		const purchase_order = data as PurchaseOrder;
 
-    if (lastEntry) {
-      data.pocode = MiscUtil.getNextNumber("PUR", lastEntry.pocode, 5);
-    } else {
-      data.pocode = MiscUtil.getNextNumber("PUR", undefined, 5);
-    }
+		// set created employee
+		purchase_order.employeeId = session.data.employeeId;
 
-    // create a purchase order object
-    const purchase_order = data as PurchaseOrder;
+		try {
+			// save entry
+			const entry = await getRepository(PurchaseOrder).save(purchase_order);
 
-    // set created employee
-    purchase_order.employeeId = session.data.employeeId;
+			// set empty array for purchase order materials
+			const purchaseOrderMaterials = [];
 
+			data.purchaseOrderMaterials.forEach((pom) => {
+				const purchaseOrderMaterial = new PurchaseOrderMaterial();
+				purchaseOrderMaterial.purchaseOrderId = entry.id;
+				purchaseOrderMaterial.materialId = pom.materialId;
+				purchaseOrderMaterial.purchasePrice = pom.purchasePrice;
+				purchaseOrderMaterial.qty = pom.qty;
+				purchaseOrderMaterial.lineTotal = pom.lineTotal;
+				purchaseOrderMaterial.unitTypeId = pom.unitTypeId;
+				purchaseOrderMaterials.push(purchaseOrderMaterial);
+			});
 
-    try {
-      // save entry
-      const entry = await getRepository(PurchaseOrder).save(purchase_order);
+			// save purchase order materials
+			await getRepository(PurchaseOrderMaterial).save(purchaseOrderMaterials);
 
-      // set empty array for purchase order materials
-      const purchaseOrderMaterials = [];
+			// // mark quotation as completed
+			// const quotationCompletedStatus = await getRepository(QuotationStatus).findOne({ where: { name: "Completed" } });
 
-      data.purchaseOrderMaterials.forEach(pom => {
-        const purchaseOrderMaterial = new PurchaseOrderMaterial();
-        purchaseOrderMaterial.purchaseOrderId = entry.id;
-        purchaseOrderMaterial.materialId = pom.materialId;
-        purchaseOrderMaterial.purchasePrice = pom.purchasePrice;
-        purchaseOrderMaterial.qty = pom.qty;
-        purchaseOrderMaterial.lineTotal = pom.lineTotal;
-        purchaseOrderMaterial.unitTypeId = pom.unitTypeId;
-        purchaseOrderMaterials.push(purchaseOrderMaterial);
-      });
+			// const quotation = await getRepository(Quotation).findOne(entry.quotationId);
+			// quotation.quotationStatus = quotationCompletedStatus;
 
-      // save purchase order materials
-      await getRepository(PurchaseOrderMaterial).save(purchaseOrderMaterials);
+			// await getRepository(Quotation).save(quotation);
 
+			// create pdf and send mail
+			await this.emailPDF(entry.id);
 
-      // // mark quotation as completed
-      // const quotationCompletedStatus = await getRepository(QuotationStatus).findOne({ where: { name: "Completed" } });
+			// send success response
+			return {
+				status: true,
+				data: { pocode: entry.pocode },
+				msg: "Purchase order has been created!",
+			};
+		} catch (e) {
+			console.log(e.code, e);
 
-      // const quotation = await getRepository(Quotation).findOne(entry.quotationId);
-      // quotation.quotationStatus = quotationCompletedStatus;
+			if (e.code == "ER_DUP_ENTRY") {
+				throw {
+					status: false,
+					type: "input",
+					msg: "Purchase order already exists for the given quotation!.",
+				};
+			}
 
-      // await getRepository(Quotation).save(quotation);
+			throw {
+				status: false,
+				type: "server",
+				msg: "Server Error!. Please check logs.",
+			};
+		}
+	}
 
-      // send success response
-      return {
-        status: true,
-        data: { pocode: entry.pocode },
-        msg: "Purchase order has been created!"
-      };
+	private static async emailPDF(purchaseOrderId) {
+		const { data: purchaseOrderData } = await this.getOne({
+			id: purchaseOrderId,
+		});
 
-    } catch (e) {
-      console.log(e.code, e);
+		const supplier = purchaseOrderData.quotation.quotationRequest.supplier;
 
-      if (e.code == "ER_DUP_ENTRY") {
-        throw {
-          status: false,
-          type: "input",
-          msg: "Purchase order already exists for the given quotation!."
-        }
-      }
+		fetch("http://localhost:3000/generate_printout", {
+			method: "post",
+			body: JSON.stringify({
+				data: purchaseOrderData,
+				templateName: "purchase_order",
+			}),
+			headers: { "Content-Type": "application/json" },
+		})
+			.then((res) => res.text())
+			.then(async (html) => {
+				const browser = await puppeteer.launch();
+				const page = await browser.newPage();
+				await page.setContent(html);
+				await page.pdf({
+					printBackground: true,
+					path: "/tmp/purchase_order.pdf",
+					format: "a4",
+					margin: {
+						top: "20px",
+						bottom: "20px",
+						left: "20px",
+						right: "20px",
+					},
+				});
+				await browser.close();
 
-      throw {
-        status: false,
-        type: "server",
-        msg: "Server Error!. Please check logs."
-      }
-    }
-  }
+				await EmailController.sendMail(
+					[supplier.email],
+					"Purchase Order",
+					"Please check the attached file",
+					"",
+					[
+						{
+							filename: "purchase_order.pdf",
+							path: "/tmp/purchase_order.pdf",
+							contentType: "application/pdf",
+						},
+					]
+				);
+			});
+	}
 
-  static async update(data) {
-    // check if an entry is present with given id
-    const selectedEntry = await getRepository(PurchaseOrder).findOne(data.id).catch(e => {
-      console.log(e.code, e);
-      throw {
-        status: false,
-        type: "server",
-        msg: "Server Error!. Please check logs."
-      }
-    });
+	static async update(data) {
+		// check if an entry is present with given id
+		const selectedEntry = await getRepository(PurchaseOrder)
+			.findOne(data.id)
+			.catch((e) => {
+				console.log(e.code, e);
+				throw {
+					status: false,
+					type: "server",
+					msg: "Server Error!. Please check logs.",
+				};
+			});
 
-    if (!selectedEntry) {
-      throw {
-        status: false,
-        type: "input",
-        msg: "That entry doesn't exist in our database!."
-      }
-    }
+		if (!selectedEntry) {
+			throw {
+				status: false,
+				type: "input",
+				msg: "That entry doesn't exist in our database!.",
+			};
+		}
 
-    const editedEntry = data as PurchaseOrder;
+		const editedEntry = data as PurchaseOrder;
 
-    try {
-      // remove existing purchase order materials
-      await getRepository(PurchaseOrderMaterial).createQueryBuilder()
-        .delete()
-        .where("purchaseOrderId = :id", { id: editedEntry.id })
-        .execute();
+		try {
+			// remove existing purchase order materials
+			await getRepository(PurchaseOrderMaterial)
+				.createQueryBuilder()
+				.delete()
+				.where("purchaseOrderId = :id", { id: editedEntry.id })
+				.execute();
 
-      // update purchase order
-      await getRepository(PurchaseOrder).save(editedEntry);
+			// update purchase order
+			await getRepository(PurchaseOrder).save(editedEntry);
 
-      // set empty array for purchase order materials
-      const purchaseOrderMaterials = [];
+			// set empty array for purchase order materials
+			const purchaseOrderMaterials = [];
 
-      data.purchaseOrderMaterials.forEach(pom => {
-        const purchaseOrderMaterial = new PurchaseOrderMaterial();
-        purchaseOrderMaterial.purchaseOrderId = editedEntry.id;
-        purchaseOrderMaterial.materialId = pom.materialId;
-        purchaseOrderMaterial.purchasePrice = pom.purchasePrice;
-        purchaseOrderMaterial.qty = pom.qty;
-        purchaseOrderMaterial.lineTotal = pom.lineTotal;
-        purchaseOrderMaterial.unitTypeId = pom.unitTypeId;
-        purchaseOrderMaterials.push(purchaseOrderMaterial);
-      });
+			data.purchaseOrderMaterials.forEach((pom) => {
+				const purchaseOrderMaterial = new PurchaseOrderMaterial();
+				purchaseOrderMaterial.purchaseOrderId = editedEntry.id;
+				purchaseOrderMaterial.materialId = pom.materialId;
+				purchaseOrderMaterial.purchasePrice = pom.purchasePrice;
+				purchaseOrderMaterial.qty = pom.qty;
+				purchaseOrderMaterial.lineTotal = pom.lineTotal;
+				purchaseOrderMaterial.unitTypeId = pom.unitTypeId;
+				purchaseOrderMaterials.push(purchaseOrderMaterial);
+			});
 
-      // save purchase order materials
-      await getRepository(PurchaseOrderMaterial).save(purchaseOrderMaterials);
+			// save purchase order materials
+			await getRepository(PurchaseOrderMaterial).save(purchaseOrderMaterials);
+		} catch (e) {
+			console.log(e.code, e);
+			throw e;
+		}
 
-    } catch (e) {
-      console.log(e.code, e);
-      throw e;
-    }
+		return {
+			status: true,
+			msg: "Purchase order has been updated!.",
+		};
+	}
 
-    return {
-      status: true,
-      msg: "Purchase order has been updated!."
-    }
-  }
+	static async delete({ id }) {
+		// find entry with the given id
+		const entry = await getRepository(PurchaseOrder)
+			.findOne({ id: id })
+			.catch((e) => {
+				console.log(e.code, e);
+				throw {
+					status: false,
+					type: "server",
+					msg: "Server Error!. Please check logs.",
+				};
+			});
 
-  static async delete({ id }) {
-    // find entry with the given id
-    const entry = await getRepository(PurchaseOrder).findOne({ id: id }).catch(e => {
-      console.log(e.code, e);
-      throw {
-        status: false,
-        type: "server",
-        msg: "Server Error!. Please check logs."
-      }
-    });
+		if (!entry) {
+			throw {
+				status: false,
+				type: "input",
+				msg: "That entry doesn't exist in our database!.",
+			};
+		}
 
-    if (!entry) {
-      throw {
-        status: false,
-        type: "input",
-        msg: "That entry doesn't exist in our database!."
-      }
-    }
+		// find deleted status
+		const deletedStatus = await getRepository(PurchaseOrderStatus)
+			.findOne({ name: "Deleted" })
+			.catch((e) => {
+				console.log(e.code, e);
+				throw {
+					status: false,
+					type: "server",
+					msg: "Server Error!. Please check logs.",
+				};
+			});
 
-    // find deleted status
-    const deletedStatus = await getRepository(PurchaseOrderStatus).findOne({ name: "Deleted" }).catch(e => {
-      console.log(e.code, e);
-      throw {
-        status: false,
-        type: "server",
-        msg: "Server Error!. Please check logs."
-      }
-    });
+		// if there is no status called deleted
+		if (!deletedStatus) {
+			throw {
+				status: false,
+				type: "server",
+				msg: "Deleted status doesn't exist in the database!.",
+			};
+		}
 
-    // if there is no status called deleted
-    if (!deletedStatus) {
-      throw {
-        status: false,
-        type: "server",
-        msg: "Deleted status doesn't exist in the database!."
-      }
-    }
+		// set status to delete
+		entry.purchaseOrderStatus = deletedStatus;
 
-    // set status to delete
-    entry.purchaseOrderStatus = deletedStatus;
+		await getRepository(PurchaseOrder)
+			.save(entry)
+			.catch((e) => {
+				console.log(e.code, e);
+				throw {
+					status: false,
+					type: "server",
+					msg: "Server Error!. Please check logs.",
+				};
+			});
 
-    await getRepository(PurchaseOrder).save(entry).catch(e => {
-      console.log(e.code, e);
-      throw {
-        status: false,
-        type: "server",
-        msg: "Server Error!. Please check logs."
-      }
-    });
+		return {
+			status: true,
+			msg: "That purchase order has been deleted!",
+		};
+	}
 
-    return {
-      status: true,
-      msg: "That purchase order has been deleted!"
-    };
-  }
+	// find purchase orders belong to single supplier
+	static async getSupplierPurchaseOrders({
+		supplierId,
+		purchaseOrderStatusName,
+	}) {
+		let entires = await PurchaseOrderDao.getSupplierPurchaseOrders(
+			supplierId,
+			purchaseOrderStatusName
+		).catch((e) => {
+			console.log(e.code, e);
+			throw {
+				status: false,
+				type: "server",
+				msg: "Server Error!. Please check logs.",
+			};
+		});
 
-  // find purchase orders belong to single supplier
-  static async getSupplierPurchaseOrders({ supplierId, purchaseOrderStatusName }) {
-    let entires = await PurchaseOrderDao.getSupplierPurchaseOrders(supplierId, purchaseOrderStatusName).catch(e => {
-      console.log(e.code, e);
-      throw {
-        status: false,
-        type: "server",
-        msg: "Server Error!. Please check logs."
-      }
-    });
-
-    return {
-      data: entires,
-      status: true
-    };
-  }
-
+		return {
+			data: entires,
+			status: true,
+		};
+	}
 }
